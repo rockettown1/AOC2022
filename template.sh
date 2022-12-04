fe04e90fb3a48fdfe696fa8e8392621985d0db61
Dan Krishnan <dhani.krishnan@theverygroup.com>
2022-12-04 14:16:13 +0000

    chore: update template script

--- template.sh
+++ template.sh
@@ -7,10 +7,14 @@ do
         d) day=${OPTARG};;
     esac
 done
+green='\033[0;32m'
+blue='\033[0;34m'
 
 cd "$language"
 mkdir -p day"$day"
 cp -r ./template/. day"$day"
+echo "${green}Create solution directory for day$day in $language"
 
 cd ../_inputs
 touch day"$day".txt
+echo "${blue}Created txt file for input if it wasn't already there."
