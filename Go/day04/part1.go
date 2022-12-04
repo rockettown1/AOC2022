9a83ae7c3b92d2de3280f18c5241135d4c3005e8
Dan Krishnan <dhani.krishnan@theverygroup.com>
2022-12-04 22:48:51 +0000

    refactor: day04 Go

--- Go/day04/part1.go
+++ Go/day04/part1.go
@@ -5,35 +5,17 @@ import (
 	"strings"
 )
 
-/*
-This is a working solution but I have no idea if it's idomatic Go code.
-I expect to come back to this after getting more experience with Go.
-*/
-
 func part1() int {
 	content := utils.GetPuzzleInput("day04")
-	lines := make([][][]int, 0)
 	c := 0
 	for _, line := range content {
-		temp := strings.Split(line, ",")
-		p := make([][]int, 0)
+		parts := strings.Split(line, ",")
 
-		for _, v := range temp {
-			arr := strings.Split(v, "-")
-			nums := utils.ToInts(arr)
-			p = append(p, nums)
-		}
-
-		lines = append(lines, p)
-	}
-
-	//compare to find fully contained and update count
-	for _, line := range lines {
-		a1, a2, b1, b2 := line[0][0], line[0][1], line[1][0], line[1][1]
-
-		if (a1 <= b1 && a2 >= b2) || (b1 <= a1 && b2 >= a2) {
+		n1, n2 := utils.ToInts(strings.Split(parts[0], "-")), utils.ToInts(strings.Split(parts[1], "-"))
+		if (n1[0] <= n2[0] && n1[1] >= n2[1]) || (n2[0] <= n1[0] && n2[1] >= n1[1]) {
 			c++
 		}
+
 	}
 
 	return c
